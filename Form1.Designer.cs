52baef677df60842987e2d1b8bf20c48dcbe5419
phi1235 <phipari12345@gmail.com>
2023-10-09 01:17:35 +0700

    LTUD

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace bt8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbKq = new ListBox();
+            txtNhap = new TextBox();
+            btnNhap = new Button();
+            btnTong = new Button();
+            btnXoa = new Button();
+            btnXoachon = new Button();
+            button5 = new Button();
+            btnThay = new Button();
+            btnChonSoChan = new Button();
+            btnChonSoLe = new Button();
+            btnKetThuc = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lbKq
+            // 
+            lbKq.FormattingEnabled = true;
+            lbKq.ItemHeight = 15;
+            lbKq.Location = new Point(30, 122);
+            lbKq.Name = "lbKq";
+            lbKq.Size = new Size(120, 169);
+            lbKq.TabIndex = 2;
+            // 
+            // txtNhap
+            // 
+            txtNhap.Location = new Point(30, 64);
+            txtNhap.Name = "txtNhap";
+            txtNhap.Size = new Size(120, 23);
+            txtNhap.TabIndex = 3;
+            // 
+            // btnNhap
+            // 
+            btnNhap.Location = new Point(30, 93);
+            btnNhap.Name = "btnNhap";
+            btnNhap.Size = new Size(120, 23);
+            btnNhap.TabIndex = 4;
+            btnNhap.Text = "Nhap";
+            btnNhap.UseVisualStyleBackColor = true;
+            btnNhap.Click += btnNhap_Click;
+            // 
+            // btnTong
+            // 
+            btnTong.Location = new Point(189, 63);
+            btnTong.Name = "btnTong";
+            btnTong.Size = new Size(194, 23);
+            btnTong.TabIndex = 5;
+            btnTong.Text = "Tong cac phan tu trong List";
+            btnTong.UseVisualStyleBackColor = true;
+            btnTong.Click += btnTong_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(189, 95);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(194, 23);
+            btnXoa.TabIndex = 6;
+            btnXoa.Text = "Xoa cac phan tu dau va cuoi";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += button3_Click;
+            // 
+            // btnXoachon
+            // 
+            btnXoachon.Location = new Point(189, 128);
+            btnXoachon.Name = "btnXoachon";
+            btnXoachon.Size = new Size(194, 23);
+            btnXoachon.TabIndex = 7;
+            btnXoachon.Text = "Xoa phan tu dang chon";
+            btnXoachon.UseVisualStyleBackColor = true;
+            btnXoachon.Click += btnXoachon_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(189, 162);
+            button5.Name = "button5";
+            button5.Size = new Size(194, 23);
+            button5.TabIndex = 8;
+            button5.Text = "Tang moi phan tu len 2";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // btnThay
+            // 
+            btnThay.Location = new Point(189, 196);
+            btnThay.Name = "btnThay";
+            btnThay.Size = new Size(194, 23);
+            btnThay.TabIndex = 9;
+            btnThay.Text = "Thay bang binh phuong";
+            btnThay.UseVisualStyleBackColor = true;
+            btnThay.Click += btnThay_Click;
+            // 
+            // btnChonSoChan
+            // 
+            btnChonSoChan.Location = new Point(189, 230);
+            btnChonSoChan.Name = "btnChonSoChan";
+            btnChonSoChan.Size = new Size(194, 23);
+            btnChonSoChan.TabIndex = 10;
+            btnChonSoChan.Text = "Chon so chan";
+            btnChonSoChan.UseVisualStyleBackColor = true;
+            btnChonSoChan.Click += btnChonSoChan_Click;
+            // 
+            // btnChonSoLe
+            // 
+            btnChonSoLe.Location = new Point(189, 262);
+            btnChonSoLe.Name = "btnChonSoLe";
+            btnChonSoLe.Size = new Size(194, 23);
+            btnChonSoLe.TabIndex = 11;
+            btnChonSoLe.Text = "Chon so le";
+            btnChonSoLe.UseVisualStyleBackColor = true;
+            btnChonSoLe.Click += btnChonSoLe_Click;
+            // 
+            // btnKetThuc
+            // 
+            btnKetThuc.Location = new Point(30, 307);
+            btnKetThuc.Name = "btnKetThuc";
+            btnKetThuc.Size = new Size(353, 23);
+            btnKetThuc.TabIndex = 12;
+            btnKetThuc.Text = "Ket thuc";
+            btnKetThuc.UseVisualStyleBackColor = true;
+            btnKetThuc.Click += btnKetThuc_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkOrange;
+            label1.Location = new Point(135, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 45);
+            label1.TabIndex = 13;
+            label1.Text = "LISTBOX";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(418, 342);
+            Controls.Add(label1);
+            Controls.Add(btnKetThuc);
+            Controls.Add(btnChonSoLe);
+            Controls.Add(btnChonSoChan);
+            Controls.Add(btnThay);
+            Controls.Add(button5);
+            Controls.Add(btnXoachon);
+            Controls.Add(btnXoa);
+            Controls.Add(btnTong);
+            Controls.Add(btnNhap);
+            Controls.Add(txtNhap);
+            Controls.Add(lbKq);
+            MinimumSize = new Size(30, 30);
+            Name = "Form1";
+            Text = " ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ListBox lbKq;
+        private TextBox txtNhap;
+        private Button btnNhap;
+        private Button btnTong;
+        private Button btnXoa;
+        private Button btnXoachon;
+        private Button button5;
+        private Button btnThay;
+        private Button btnChonSoChan;
+        private Button btnChonSoLe;
+        private Button btnKetThuc;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System.Windows.Forms;
+
+namespace bt8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem có ít nhất một phần tử trong ListBox
+            if (lbKq.Items.Count > 0)
+            {
+                // Xóa phần tử đầu tiên
+                lbKq.Items.RemoveAt(0);
+
+                // Kiểm tra xem còn ít nhất một phần tử nữa trong ListBox sau khi xóa phần tử đầu tiên
+                if (lbKq.Items.Count > 0)
+                {
+                    // Xóa phần tử cuối cùng (vì ListBox1.Items.Count đã giảm đi 1 sau lệnh xóa đầu tiên)
+                    lbKq.Items.RemoveAt(lbKq.Items.Count - 1);
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            // Lấy số từ TextBox
+            string inputText = txtNhap.Text;
+
+            // Kiểm tra nếu TextBox không trống
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                // Thêm số vào ListBox
+                lbKq.Items.Add(inputText);
+
+                // Xóa nội dung trong TextBox và đặt con trỏ vào TextBox
+                txtNhap.Clear();
+                txtNhap.Focus();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dat con tro chuot khi load form
+            txtNhap.Focus();
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            // Hiển thị số lượng phần tử trong ListBox bằng MessageBox
+            MessageBox.Show("Số lượng phần tử trong ListBox: " + lbKq.Items.Count, "Kết quả");
+        }
+
+        private void btnXoachon_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem có ít nhất một phần tử đã được chọn
+            if (lbKq.SelectedIndex != -1)
+            {
+                // Xóa phần tử đang được chọn
+                lbKq.Items.RemoveAt(lbKq.SelectedIndex);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Duyệt qua tất cả các phần tử trong ListBox
+            for (int i = 0; i < lbKq.Items.Count; i++)
+            {
+                // Lấy giá trị của phần tử hiện tại
+                int giaTriHienTai = int.Parse(lbKq.Items[i].ToString());
+
+                // Tăng giá trị lên 2
+                giaTriHienTai += 2;
+
+                // Cập nhật lại giá trị của phần tử trong ListBox
+                lbKq.Items[i] = giaTriHienTai.ToString();
+            }
+        }
+
+        private void btnThay_Click(object sender, EventArgs e)
+        {
+            // Duyệt qua tất cả các phần tử trong ListBox
+            for (int i = 0; i < lbKq.Items.Count; i++)
+            {
+                // Lấy giá trị của phần tử hiện tại
+                int giaTriHienTai = int.Parse(lbKq.Items[i].ToString());
+
+                // Tính bình phương của giá trị
+                int binhPhuong = giaTriHienTai * giaTriHienTai;
+
+                // Cập nhật lại giá trị của phần tử trong ListBox với giá trị bình phương
+                lbKq.Items[i] = binhPhuong.ToString();
+            }
+        }
+
+        private void btnChonSoChan_Click(object sender, EventArgs e)
+        {
+            // Tạo một danh sách để lưu trữ các số chẵn đã được chọn
+            List<int> soChanDaChon = new List<int>();
+
+            // Duyệt qua tất cả các phần tử trong ListBox
+            foreach (var item in lbKq.Items)
+            {
+                // Lấy giá trị của phần tử hiện tại và chuyển đổi nó thành số nguyên
+                if (int.TryParse(item.ToString(), out int giaTri))
+                {
+                    // Kiểm tra xem số đó có phải là số chẵn hay không
+                    if (giaTri % 2 == 0)
+                    {
+                        // Nếu là số chẵn, thêm vào danh sách số chẵn đã chọn
+                        soChanDaChon.Add(giaTri);
+                    }
+                }
+            }
+
+            // Hiển thị các số chẵn đã chọn trong một MessageBox
+            if (soChanDaChon.Count > 0)
+            {
+                MessageBox.Show("Các số chẵn đã chọn: " + string.Join(", ", soChanDaChon), "Số chẵn");
+            }
+            else
+            {
+                MessageBox.Show("Không có số chẵn nào được chọn.", "Số chẵn");
+            }
+        }
+
+        private void btnChonSoLe_Click(object sender, EventArgs e)
+        {
+            // Tạo một danh sách để lưu trữ các số le đã được chọn
+            List<int> soLeDaChon = new List<int>();
+
+            // Duyệt qua tất cả các phần tử trong ListBox
+            foreach (var item in lbKq.Items)
+            {
+                // Lấy giá trị của phần tử hiện tại và chuyển đổi nó thành số nguyên
+                if (int.TryParse(item.ToString(), out int giaTri))
+                {
+                    // Kiểm tra xem số đó có phải là số le hay không
+                    if (giaTri % 2 != 0)
+                    {
+                        // Nếu là số le, thêm vào danh sách số le đã chọn
+                        soLeDaChon.Add(giaTri);
+                    }
+                }
+            }
+
+            // Hiển thị các số le đã chọn trong một MessageBox
+            if (soLeDaChon.Count > 0)
+            {
+                MessageBox.Show("Các số le đã chọn: " + string.Join(", ", soLeDaChon), "Số le");
+            }
+            else
+            {
+                MessageBox.Show("Không có số le nào được chọn.", "Số le");
+            }
+        }
+
+        private void btnKetThuc_Click(object sender, EventArgs e)
+        {
+            DialogResult kq = MessageBox.Show("Ban chac chan muon thoat khong?","Thong bao",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (kq == DialogResult.Yes)
+            {
+                this.Close();
+            }
+           
+        }
+    }
+}
